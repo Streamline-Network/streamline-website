1320bf3ed1e245a66410cf29993a8b5dee05f785
Hexy32 <sunshine.goody8762@gmail.com>
2022-11-01 23:29:28 -0400

    Finished header styling

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -2,6 +2,7 @@ import classnames from 'classnames'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React from 'react'
+import style from './header.module.scss'
 
 export default function Header({}: HeaderProps) {
   const pages = [
@@ -14,28 +15,23 @@ export default function Header({}: HeaderProps) {
   const path = usePathname()
 
   return (
-    <header>
-      <div className="header">
-        <Link className="title" href="/">
-          Streamline SMP
-        </Link>
-        <nav className="main-navigation" role="navigation">
-          <span className="line first-line"></span>
-          <span className="line secound-line"></span>
-          <span className="line third-line"></span>
-          <ul className="nav-list">
-            {pages.map(([name, url], i) => (
-              <li
-                key={i}
-                className={classnames({ 'current-pg': url === path })}
-              >
-                <Link href={url}>{name}</Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      </div>
-      <div className="notifications"></div>
+    <header className={style.header}>
+      <Link href="/">Streamline SMP</Link>
+      <nav role="navigation">
+        <span></span>
+        <span></span>
+        <span></span>
+        <ul>
+          {pages.map(([name, url], i) => (
+            <li
+              key={i}
+              className={classnames({ [style['current-pg']]: url === path })}
+            >
+              <Link href={url}>{name}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </header>
   )
 }
