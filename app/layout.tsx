bc1928a3dfe95d1b60365be6b98bafa02d274d98
Ascor8522 <vinoa.89.jeux@gmail.com>
2022-11-02 18:01:47 +0100

    Fixed build (don't use <Head>)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import '/styles/style.scss';
 
 import { Rubik } from '@next/font/google';
-import Head from "next/head";
 import React from 'react';
 
 import Banner, { BannerProps } from './components/banner/banner';
@@ -18,15 +17,13 @@ export default function Layout({ children }: LayoutProps) {
 	const messages: BannerProps[] = [
 		{
 			title: '',
-			message:
-				'We are currently in between seasons and are not accepting new people. Expect season four summer of 2023.',
+			message: 'We are currently in between seasons and are not accepting new people. Expect season four summer of 2023.',
 			color: '',
 		},
 	];
 	return (
-		<html className={rubik.className}>
-			<Head><></></Head>
-			<body>
+		<html>
+			<body className={rubik.className}>
 				<Header />
 				{/*messages.map((message, index) => (
 					<Banner key={index} {...message} />
