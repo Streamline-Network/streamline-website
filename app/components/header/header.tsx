59a263b5ae0fd97fbb9a28811911987c6f24c4cf
Hexy32 <sunshine.goody8762@gmail.com>
2022-11-06 12:22:54 -0500

    Fixed mobile nav

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -1,46 +1,15 @@
-"use client";
-
-import classnames from "classnames";
-import Image from "next/image";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import Nav from "./nav/nav";
 import React from "react";
-
 import header from "./header.module.scss";
-import hamburger from "./images/hamburger.png";
 
-export default function Header({ }: HeaderProps) {
-	const pages = [
-		["Home", "/"],
-		["Community", "/community"],
-		["About", "/about"],
-		["Join", "/join"],
-	];
-
-	const path = usePathname();
-	const [isOpen, setIsOpen] = React.useState(false);
-	const toggle = () => setIsOpen(isOpen => !isOpen);
-
+export default function Header({}: HeaderProps) {
 	return (
 		<header className={header.header}>
 			<Link href="/">Streamline SMP</Link>
-			<nav className={classnames({ [header.open]: isOpen })}>
-				<ul>
-					{pages.map(([name, url], i) => (
-						<li
-							key={i}
-							className={classnames({ [header.currentPg]: url === path })}
-						>
-							<Link href={url}>{name}</Link>
-						</li>
-					))}
-				</ul>
-				<button onClick={toggle}>
-					<Image src={hamburger} alt="Menu" width={40} />
-				</button>
-			</nav>
+			<Nav />
 		</header>
 	);
 }
 
-interface HeaderProps { }
+interface HeaderProps {}
---
+++ app/components/header/nav/nav.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import Link from "next/link";
+import React from "react";
+import classnames from "classnames";
+import header from "../header.module.scss";
+import { usePathname } from "next/navigation";
+
+export default function Nav({}: NavProps) {
+	const pages = [
+		["Home", "/"],
+		["Community", "/community"],
+		["About", "/about"],
+		["Join", "/join"],
+	];
+
+	const path = usePathname();
+	const [isOpen, setIsOpen] = React.useState(false);
+	const toggle = () => setIsOpen((isOpen) => !isOpen);
+
+	return (
+		<nav className={classnames({ [header.open]: isOpen })}>
+			<button onClick={toggle}>
+				<span />
+				<span />
+				<span />
+			</button>
+			<ul>
+				{pages.map(([name, url], i) => (
+					<li key={i} className={classnames({ [header.currentPg]: url === path })}>
+						<Link href={url}>{name}</Link>
+					</li>
+				))}
+			</ul>
+		</nav>
+	);
+}
+
+interface NavProps {}
