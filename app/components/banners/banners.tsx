14c9c7cfcf2268d001d80e8fee06a33f25f297ca
Hexy32 <sunshine.goody8762@gmail.com>
2022-11-06 01:42:19 -0400

    Finished banners
    Co-authored-by: Ascor8522 <vinoa.89.jeux@gmail.com>

--- app/components/banners/banners.tsx
+++ app/components/banners/banners.tsx
@@ -1,47 +1,26 @@
 "use client";
 
-import { useState } from "react";
-
 import Banner, { BannerProps } from "./banner/banner";
-import banners from "./banners.module.scss";
+
+import banners from "./banners.json";
+import { useState } from "react";
 
 export default function Banners() {
 	type Messages = Omit<BannerProps, "close" | "index">[];
 
-	const [messages, setMessages] = useState<Messages>([
-		{
-			message: "We are currently in between seasons and are not accepting new people. Season 4 releases November the 11th!",
-			id: "in-between-seasons-3-4",
-		},
-		{
-			message: "Eiusmod officia sunt enim elit sint amet aliquip.",
-			id: "Occaecat",
-		},
-		{
-			message: "Minim in proident ad aliquip est ad adipisicing dolore.",
-			id: "Nulla",
-		},
-		{
-			message: "Aliquip non proident quis id qui cupidatat mollit ea sunt in minim aute tempor.",
-			id: "Mollit",
-		},
-		{
-			message: "Ea officia cupidatat pariatur irure laboris exercitation anim.",
-			id: "Eiusmod",
-		},
-		{
-			message: "Exercitation sunt velit in labore voluptate sit minim dolor laborum ipsum qui.",
-			id: "Enim",
-		},
-		{
-			message: "Laboris ex incididunt quis magna exercitation ex labore adipisicing.",
-			id: "Eiusmod2",
-		},
-	]);
+	const lsKey = "closedBannerIds";
 
-	const close = (id: string) =>
-		setMessages(messages =>
-			[...messages.filter(message => message.id !== id)]);
+	const existing = banners.map(({ id }) => id);
+	const closed: string[] = JSON.parse(localStorage.getItem(lsKey) ?? "[]");
+	const existingClosed = closed.filter((id) => existing.includes(id));
+	const bannersToDisplay = banners.filter((banner) => !closed.includes(banner.id));
+
+	const [messages, setMessages] = useState<Messages>(bannersToDisplay);
+
+	const close = (id: string) => {
+		setMessages((messages) => [...messages.filter((message) => message.id !== id)]);
+		localStorage.setItem(lsKey, JSON.stringify([...existingClosed, id]));
+	};
 
 	return (
 		<>
