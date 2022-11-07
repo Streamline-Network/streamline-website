42ab79371db1752e366aa3ce8cc9455b4125a8fa
Ascor8522 <vinoa.89.jeux@gmail.com>
2022-11-07 21:35:55 +0100

    Cleaned code for the banner

--- app/components/banners/banners.tsx
+++ app/components/banners/banners.tsx
@@ -1,33 +1,31 @@
 "use client";
 
-import Banner, { BannerProps } from "./banner/banner";
-
-import banners from "./banners.json";
 import { useState } from "react";
+
+import Banner, { BannerProps } from "./banner/banner";
+import banners from "./banners.json";
 
 export default function Banners() {
 	type Messages = Omit<BannerProps, "close" | "index">[];
 	const lsKey = "closedBannerIds";
 
-	const lsAccess = (setting?: boolean, item?: string[]) => {
-		if (typeof window === "undefined") return [];
-		if (setting) {
-			localStorage.setItem(lsKey, JSON.stringify(item));
-		} else {
-			return JSON.parse(localStorage.getItem(lsKey) || "[]");
-		}
+	const getOrSetLS = (item?: string[]): string[] => {
+		if(typeof window === "undefined") return [];
+		return item
+			? localStorage.setItem(lsKey, JSON.stringify(item))
+			: JSON.parse(localStorage.getItem(lsKey) || "[]");
 	};
 
-	const existing = banners.map(({ id }) => id);
-	const closed: string[] = lsAccess();
-	const existingClosed = closed.filter((id) => existing.includes(id));
-	const bannersToDisplay = banners.filter((banner) => !closed.includes(banner.id));
+	const existingIds = banners.map(({ id }) => id);
+	const closedIds = getOrSetLS();
+	const existingAndClosedIds = closedIds.filter((id) => existingIds.includes(id));
+	const bannersToDisplay = banners.filter((banner) => !closedIds.includes(banner.id));
 
 	const [messages, setMessages] = useState<Messages>(bannersToDisplay);
 
 	const close = (id: string) => {
 		setMessages((messages) => [...messages.filter((message) => message.id !== id)]);
-		lsAccess(true, [...existingClosed, id]);
+		getOrSetLS([...existingAndClosedIds, id]);
 	};
 
 	return (
