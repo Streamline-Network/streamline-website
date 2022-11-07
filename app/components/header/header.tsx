e8ddc2cb59ccf19050a6c02f3a6ed28382a3d079
Ascor8522 <vinoa.89.jeux@gmail.com>
2022-11-07 21:27:26 +0100

    Made the header 1 fiel again

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -1,13 +1,53 @@
-import Nav from "./nav/nav";
-import React from "react";
+"use client";
+
+import classnames from "classnames";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React, { MouseEvent } from "react";
+
 import header from "./header.module.scss";
 
-export default function Header({}: HeaderProps) {
+export default function Header({ }: HeaderProps) {
+	const pages = [
+		["Home", "/"],
+		["Community", "/community"],
+		["About", "/about"],
+		["Join", "/join"],
+	];
+
+	const path = usePathname();
+	const [isOpen, setIsOpen] = React.useState(false);
+	const toggle = (_event?: MouseEvent, close?: true) => {
+		setIsOpen((isOpen) => {
+			document && (document.body.style.overflow = (document && isOpen) ? "" : "hidden"); // TODO: use CSS :has() selector
+			return close ? false : !isOpen;
+		});
+	};
+
 	return (
 		<header className={header.header}>
-			<Nav />
+			<Link onClick={() => toggle(undefined, true)} href="/">
+				Streamline SMP
+			</Link>
+
+			<nav className={classnames({ [header.open]: isOpen })}>
+				<button onClick={toggle}>
+					<span />
+					<span />
+					<span />
+				</button>
+				<ul>
+					{pages.map(([name, url], i) => (
+						<li key={i} className={classnames({ [header.currentPg]: url === path })}>
+							<Link onClick={() => toggle(undefined, true)} href={url}>
+								{name}
+							</Link>
+						</li>
+					))}
+				</ul>
+			</nav>
 		</header>
 	);
 }
 
-interface HeaderProps {}
+interface HeaderProps { }
