d89ec5b6dc0126170bea7cf8fca4f56732c2a73b
Hexy32 <sunshine.goody8762@gmail.com>
2022-12-17 15:43:49 -0500

    Changed banner

--- app/components/banners/banners.tsx
+++ app/components/banners/banners.tsx
@@ -1,31 +1,39 @@
 "use client";
 
-import { useState } from "react";
-
 import Banner, { BannerProps } from "./banner/banner";
+
 import banners from "./banners.json";
+import { useState } from "react";
 
 export default function Banners() {
 	type Messages = Omit<BannerProps, "close" | "index">[];
 	const lsKey = "closedBannerIds";
 
 	const getOrSetLS = (item?: string[]): string[] => {
-		if(typeof window === "undefined") return [];
+		if (typeof window === "undefined") return [];
 		return item
 			? localStorage.setItem(lsKey, JSON.stringify(item))
 			: JSON.parse(localStorage.getItem(lsKey) || "[]");
 	};
 
-	const existingIds = banners.map(({ id }) => id);
-	const closedIds = getOrSetLS();
-	const existingAndClosedIds = closedIds.filter((id) => existingIds.includes(id));
-	const bannersToDisplay = banners.filter((banner) => !closedIds.includes(banner.id));
+	const getBannerInfo = () => {
+		const existingIds = banners.map(({ id }) => id);
+		const closedIds = getOrSetLS();
+		const existingAndClosedIds = closedIds.filter((id) => existingIds.includes(id));
+		const bannersToDisplay = banners.filter((banner) => !closedIds.includes(banner.id));
+		return {
+			bannersToDisplay: bannersToDisplay,
+			existingAndClosedIds: existingAndClosedIds,
+		};
+	};
 
-	const [messages, setMessages] = useState<Messages>(bannersToDisplay);
+	const bannerInfo = getBannerInfo();
+
+	const [messages, setMessages] = useState<Messages>(bannerInfo.bannersToDisplay);
 
 	const close = (id: string) => {
 		setMessages((messages) => [...messages.filter((message) => message.id !== id)]);
-		getOrSetLS([...existingAndClosedIds, id]);
+		getOrSetLS([...bannerInfo.existingAndClosedIds, id]);
 	};
 
 	return (
