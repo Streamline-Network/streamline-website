6f5efa667d08c913b4246ea84fbb6b791d559ef6
Hexy32 <sunshine.goody8762@gmail.com>
2023-02-04 13:15:44 -0500

    map test

--- pages/map/index.tsx
+++ pages/map/index.tsx
@@ -5,20 +5,6 @@ import Link from 'next/link'
 import map from './map.module.scss'
 
 export default function Map() {
-  /*
-	const url = process.env.MAP_URL || "";
-	const url = "https://example.com";
-	const iframeRef = useRef<HTMLIFrameElement>(null);
-
-	useEffect(() => {
-		console.log(iframeRef.current?.contentWindow);
-
-		iframeRef.current?.contentWindow?.addEventListener("DOMContentLoaded", () => {
-			console.log("something in the console");
-		});
-	}, []);
-	*/
-
   return (
     <>
       <Head>
@@ -33,7 +19,7 @@ export default function Map() {
         <h1>We apologize but this page isn&lsquo;t set-up yet.</h1>
         <Link href={'/'}>Return home</Link>
       </section>
-      {/* <iframe ref={iframeRef} className={map.mapFrame} src={url} /> */}
+      <iframe className={map.mapFrame} src={'http://dynmap.streamlinesmp.com/'} />
     </>
   )
 }
