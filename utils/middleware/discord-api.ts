60b8c76e241b7532e2b8e999ca8afcb2ae249d71
Hexy32 <sunshine.goody8762@gmail.com>
2023-03-30 15:43:15 -0400

    more debuging

--- utils/middleware/discord-api.ts
+++ utils/middleware/discord-api.ts
@@ -6,14 +6,17 @@ export function verifyDiscordRequest(req: NextApiRequest) {
   const signature = req.headers['x-signature-ed25519'] as string
   const timestamp = req.headers['x-signature-timestamp'] as string
 
-  db.doc('/other/' + timestamp ?? 'defaulted').set({ signature: signature ?? req.headers, body: req.body })
-
   const isValidRequest = verifyKey(
     req.body,
     signature,
     timestamp,
     process.env.DISCORD_CLIENT_PUBLIC!
   )
+  db.doc('/other/' + timestamp ?? 'defaulted').set({
+    signature: signature ?? req.headers,
+    body: req.body,
+    isValidRequest,
+  })
 
   return isValidRequest
 }
