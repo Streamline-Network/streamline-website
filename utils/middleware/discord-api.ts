067dd8110b4e8ac7d5aca44d52bb65b50d7f6a77
Hexy32 <sunshine.goody8762@gmail.com>
2023-03-30 14:46:52 -0400

    Small fix

--- utils/middleware/discord-api.ts
+++ utils/middleware/discord-api.ts
@@ -2,8 +2,8 @@ import { NextApiRequest } from 'next'
 import { verifyKey } from 'discord-interactions'
 
 export function verifyDiscordRequest(req: NextApiRequest) {
-  const signature = req.headers['X-Signature-Ed25519'] as string
-  const timestamp = req.headers['X-Signature-Timestamp'] as string
+  const signature = req.headers['x-signature-ed25519'] as string
+  const timestamp = req.headers['x-signature-timestamp'] as string
 
   const isValidRequest = verifyKey(req.body, signature, timestamp, process.env.DISCORD_PUBLIC_KEY!)
 
