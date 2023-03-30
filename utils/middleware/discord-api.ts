6bc63d5190304698aefc6f4fedea587029822bf9
Hexy32 <sunshine.goody8762@gmail.com>
2023-03-30 15:11:27 -0400

    Debug stuff added

--- utils/middleware/discord-api.ts
+++ utils/middleware/discord-api.ts
@@ -1,9 +1,12 @@
 import { NextApiRequest } from 'next'
+import { db } from 'config/firebase'
 import { verifyKey } from 'discord-interactions'
 
 export function verifyDiscordRequest(req: NextApiRequest) {
   const signature = req.headers['x-signature-ed25519'] as string
   const timestamp = req.headers['x-signature-timestamp'] as string
+
+  db.doc('/other/' + timestamp).set({ signature })
 
   const isValidRequest = verifyKey(req.body, signature, timestamp, process.env.DISCORD_PUBLIC_KEY!)
 
