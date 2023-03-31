78cbe233fb4199f12d8bf7ebfa8f36305f94f42c
Hexy32 <sunshine.goody8762@gmail.com>
2023-03-30 23:15:22 -0400

    OwO

--- utils/middleware/discord-api.ts
+++ utils/middleware/discord-api.ts
@@ -1,6 +1,5 @@
 import { NextApiRequest } from 'next'
 import { db } from 'config/firebase'
-import nacl from 'tweetnacl'
 import { verifyKey } from 'discord-interactions'
 
 export function verifyDiscordRequest(req: NextApiRequest) {
@@ -8,23 +7,16 @@ export function verifyDiscordRequest(req: NextApiRequest) {
   const timestamp = req.headers['x-signature-timestamp'] as string
 
   const isValidRequest = verifyKey(
-    JSON.stringify(req.body),
+    req.body,
     signature,
     timestamp,
     process.env.DISCORD_CLIENT_PUBLIC!
   )
 
-  const isVerified = nacl.sign.detached.verify(
-    Buffer.from(timestamp + req.body),
-    Buffer.from(signature, 'hex'),
-    Buffer.from(process.env.DISCORD_CLIENT_PUBLIC!, 'hex')
-  )
-
-  db.doc('/other/' + timestamp ?? 'defaulted').set({
-    signature: signature ?? req.headers,
+  db.doc('/other/' + timestamp).set({
+    signature: signature,
     body: req.body,
     isValidRequest,
-    isVerified,
   })
 
   return isValidRequest
