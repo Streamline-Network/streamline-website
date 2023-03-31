ed03ed63d0e949d6a0b8cee55779300789ef8241
Hexy32 <sunshine.goody8762@gmail.com>
2023-03-30 23:54:51 -0400

    nacl attempt

--- utils/middleware/discord-api.ts
+++ utils/middleware/discord-api.ts
@@ -1,25 +1,31 @@
 import { NextApiRequest } from 'next'
 import { db } from 'config/firebase'
+import nacl from 'tweetnacl'
 import { parseRawBodyAsString } from 'utils/body-parser'
 import { verifyKey } from 'discord-interactions'
 
-export async function verifyDiscordRequest(req: NextApiRequest) {
+export function verifyDiscordRequest(req: NextApiRequest) {
   const signature = req.headers['x-signature-ed25519'] as string
   const timestamp = req.headers['x-signature-timestamp'] as string
 
-  const rawBody = await parseRawBodyAsString(req)
-
   const isValidRequest = verifyKey(
-    rawBody,
+    req.body,
     signature,
     timestamp,
     process.env.DISCORD_CLIENT_PUBLIC!
   )
 
-  db.doc('/other/' + timestamp).set({
-    signature: signature,
-    body: rawBody,
+  const isVerified = nacl.sign.detached.verify(
+    Buffer.from(timestamp + parseRawBodyAsString(req)),
+    Buffer.from(signature, 'hex'),
+    Buffer.from(process.env.DISCORD_CLIENT_PUBLIC!, 'hex')
+  )
+
+  db.doc('/other/' + timestamp ?? 'defaulted').set({
+    signature: signature ?? req.headers,
+    body: req.body,
     isValidRequest,
+    isVerified,
   })
 
   return isValidRequest
