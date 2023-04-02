fdaa0c2daedb44d0ecfadf93691f485359d1fec8
Hexy32 <sunshine.goody8762@gmail.com>
2023-04-02 19:52:09 -0400

    Added some debugging

--- utils/discord/register-commands.ts
+++ utils/discord/register-commands.ts
@@ -1,15 +1,13 @@
-import {
-  RESTGetAPIApplicationCommandsResult,
-  RESTPutAPIApplicationCommandsJSONBody,
-} from 'discord-api-types/v10'
-
 import { CommandObject } from 'utils/discord/command-handler'
+import { RESTPutAPIApplicationCommandsJSONBody } from 'discord-api-types/v10'
 import customFetch from 'utils/fetch'
 import { discordAuthHeaders } from 'utils/discord/verify-discord-request'
 import fs from 'node:fs'
 
 export default async function registerCommands() {
-  const commandFiles = fs.readdirSync('utils/discord/commands').filter(file => file.endsWith('.ts'))
+  const commandFiles = fs.readdirSync('utils/discord/commands')
+
+  console.log('Found these files:', commandFiles)
 
   const commands: CommandObject[] = []
 
