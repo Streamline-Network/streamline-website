752275def8f931955c9b0d7a56c5481e16809e02
Hexy32 <sunshine.goody8762@gmail.com>
2023-04-11 17:41:42 -0400

    Added fuzzy search

--- pages/account/admin/review/index.tsx
+++ pages/account/admin/review/index.tsx
@@ -5,6 +5,7 @@ import CardSelector from 'components/fragments/review/card-selector'
 import CardSkeleton from 'components/fragments/review/card-skeletons'
 import { Database } from 'pages/api/db/database'
 import FormBlocks from 'components/fragments/blocks/form-blocks/form-blocks'
+import FuzzySearch from 'fuzzy-search'
 import Loading from 'components/fragments/application/loading'
 import classNames from 'classnames'
 import customFetch from 'utils/fetch'
@@ -12,13 +13,18 @@ import review from './review.module.scss'
 import sections from 'components/fragments/application/apply-application-data'
 
 export const PER_SECTION_LIMIT = 5
+const SEARCH_AMOUNT = 50
 
 export default function Review() {
   const [query, setQuery] = useState('')
+  const [isSearching, setIsSearching] = useState(false)
   const [applicationData, setApplicationData] = useState<Database.Applications.Apply[] | undefined>(
     undefined
   )
   const [currentApplicationUuid, setCurrentApplicationUuid] = useState<string | -1>(-1)
+  const [queriedApplicationData, setQueriedApplicationData] = useState<
+    Database.Applications.Apply[] | undefined
+  >(undefined)
   const [allLoaded, setAllLoaded] = useState(false)
   const [error, setError] = useState<string | undefined>(undefined)
 
@@ -30,8 +36,45 @@ export default function Review() {
     })
   }, [])
 
+  useEffect(() => {
+    if (!query) {
+      setAllLoaded(false)
+      setIsSearching(false)
+      setQueriedApplicationData(undefined)
+      return
+    }
+    if (!isSearching) {
+      customFetch<Database.Applications.Apply[]>(
+        `/api/db/forms/collection-group?applicationType=apply&limit=${SEARCH_AMOUNT}&direction=desc`
+      ).then(({ data }) => {
+        setApplicationData(data)
+      })
+
+      setIsSearching(true)
+      setAllLoaded(true)
+    }
+
+    if (!applicationData) return
+
+    function getAllQuestions() {
+      const questions = applicationData![0].submissionDetails.answers
+
+      let final: string[] = []
+      for (const question of Object.keys(questions)) {
+        final.push(`submissionDetails.answers.${question}`)
+      }
+
+      return final
+    }
+
+    const searcher = new FuzzySearch(applicationData, ['minecraftUuid', ...getAllQuestions()])
+    setCurrentApplicationUuid(-1)
+
+    setQueriedApplicationData(searcher.search(query))
+  }, [applicationData, isSearching, query])
+
   function loadMore() {
-    if (!applicationData || allLoaded) return
+    if (!applicationData || allLoaded || isSearching) return
 
     const oldest = applicationData[applicationData.length - 1].submissionDetails.submissionTime
 
@@ -60,7 +103,8 @@ export default function Review() {
               title: 'Search applications',
               paragraphs: [
                 <>
-                  Preform a fuzzy search on applications, search by age, name, or Minecraft name.
+                  Preform a fuzzy search on the {50} newest applications, search by age, Minecraft
+                  name, Minecraft UUID, or any question response.
                 </>,
                 <>
                   <input
@@ -77,7 +121,7 @@ export default function Review() {
         <h2 className={review.subheader}>Select an application</h2>
         {applicationData ? (
           <CardSelector
-            applications={applicationData}
+            applications={queriedApplicationData ? queriedApplicationData : applicationData}
             currentApplicationUuid={currentApplicationUuid}
             setCurrentApplicationUuid={setCurrentApplicationUuid}
             loadMore={loadMore}
