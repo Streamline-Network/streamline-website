81d4c1c329de2961af722f8739dc4318942cda95
Hexy32 <sunshine.goody8762@gmail.com>
2023-05-19 15:17:43 -0400

    Moved test stuff to test page

--- pages/rules/index.tsx
+++ pages/rules/index.tsx
@@ -11,38 +11,7 @@ export default function Rules() {
   return (
     <>
       <h1 className={classNames('orange', rules.title)}>Rules</h1>
-      <div>
-        <div>
-          <select
-            value={action}
-            onChange={e => setAction(e.target.value as 'list' | 'add' | 'remove')}>
-            <option value="add">Add</option>
-            <option value="remove">Remove</option>
-            <option value="list">List</option>
-          </select>
-          <input type="text" onChange={e => setUuid(e.target.value)} value={uuid} />
-          <input
-            onClick={() => {
-              if (action === 'list') {
-                const data: StreamlinePluginJSON = {
-                  action: 'whitelist',
-                  payload: { operation: action },
-                }
-                return setResult(JSON.stringify(data))
-              } else {
-                const data: StreamlinePluginJSON = {
-                  action: 'whitelist',
-                  payload: { operation: action, uuid },
-                }
-                return setResult(JSON.stringify(data))
-              }
-            }}
-            type="button"
-            value="Submit"
-          />
-          <p>{result}</p>
-        </div>
-      </div>
+      <div>Under Construction 🏗️</div>
     </>
   )
 }
