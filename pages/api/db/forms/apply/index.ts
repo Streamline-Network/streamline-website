1925fa6b9d08af4a1de7471c1f0065d47cf82c06
Hexy32 <sunshine.goody8762@gmail.com>
2023-05-22 14:33:01 -0400

    set application to unReviewed when re-submitted

--- pages/api/db/forms/apply/index.ts
+++ pages/api/db/forms/apply/index.ts
@@ -5,6 +5,7 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { Database } from 'pages/api/db/database'
 import { authOptions } from '../../../auth/[...nextauth]'
 import { db } from 'config/firebase'
+import { firestore } from 'firebase-admin'
 import { getServerSession } from 'next-auth'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
@@ -18,42 +19,45 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   const applicationsRef = db.doc(`applications/${session.id}/types/apply`)
 
-  await db.runTransaction(async transaction => {
-    const docSnap = await transaction.get(applicationsRef)
+  await Promise.all([
+    db.runTransaction(async transaction => {
+      const docSnap = await transaction.get(applicationsRef)
 
-    if (docSnap.exists) {
-      const previousApplication = docSnap.data() as Database.Applications.Apply
+      if (docSnap.exists) {
+        const previousApplication = docSnap.data() as Database.Applications.Apply
 
-      if (previousApplication.submissionDetails) {
-        previousApplication.comments!.push({
-          name: session.user.name,
-          senderId: session.id,
-          time: Date.now(),
-          senderPicture: session.user.image,
-          userAction: 'Updated application',
-        })
-        applicationData.comments = previousApplication.comments
+        if (previousApplication.submissionDetails) {
+          previousApplication.comments!.push({
+            name: session.user.name,
+            senderId: session.id,
+            time: Date.now(),
+            senderPicture: session.user.image,
+            userAction: 'Updated application',
+          })
+          applicationData.comments = previousApplication.comments
 
-        applicationData.previousSubmissions = previousApplication.previousSubmissions
-          ? [...previousApplication.previousSubmissions, previousApplication.submissionDetails]
-          : [previousApplication.submissionDetails]
+          applicationData.previousSubmissions = previousApplication.previousSubmissions
+            ? [...previousApplication.previousSubmissions, previousApplication.submissionDetails]
+            : [previousApplication.submissionDetails]
+        }
       }
-    }
 
-    // Make sure data is safe by only including any expected values.
-    const safeData: Database.Applications.Apply = {
-      minecraftUuid: applicationData.minecraftUuid,
-      submissionDetails: applicationData.submissionDetails,
-      type: 'apply',
-    }
+      // Make sure data is safe by only including any expected values.
+      const safeData: Database.Applications.Apply = {
+        minecraftUuid: applicationData.minecraftUuid,
+        submissionDetails: applicationData.submissionDetails,
+        type: 'apply',
+        state: firestore.FieldValue.delete() as any,
+      }
 
-    if (docSnap.exists) {
-      safeData.comments = applicationData.comments
-      safeData.previousSubmissions = applicationData.previousSubmissions
-    }
+      if (docSnap.exists) {
+        safeData.comments = applicationData.comments
+        safeData.previousSubmissions = applicationData.previousSubmissions
+      }
 
-    transaction.set(applicationsRef, safeData, { merge: true })
-  })
+      transaction.set(applicationsRef, safeData, { merge: true })
+    }),
+  ])
 
   return res.status(200).end()
 }
