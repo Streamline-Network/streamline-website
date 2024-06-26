92aed8e7cfa406482c0e6ba23feee19beeba39bb
Hexy32 <sunshine.goody8762@gmail.com>
2024-06-25 23:20:57 -0400

    Added Stats

--- pages/stats/index.tsx
+++ pages/stats/index.tsx
@@ -1,6 +1,31 @@
+import { useEffect, useState } from 'react'
+
 import { NextSeo } from 'next-seo'
+import stats from './stats.module.scss'
 
 export default function Map() {
+  const [height, setHeight] = useState('100px')
+
+  useEffect(() => {
+    window.open('http://srv34.godlike.club:26914/', '_blank')
+
+    function getHeight() {
+      const header = document.querySelector<HTMLElement>('header')!
+      const footer = document.querySelector<HTMLElement>('footer')!
+
+      setHeight(
+        window.innerHeight -
+          (header.offsetHeight + footer.offsetHeight) -
+          4 +
+          'px'
+      )
+    }
+
+    window.addEventListener('resize', getHeight)
+    getHeight()
+
+    return () => window.removeEventListener('resize', getHeight)
+  }, [])
   return (
     <>
       <NextSeo
@@ -8,14 +33,18 @@ export default function Map() {
         description="Track your progress on Streamline SMP, a vanilla whitelist-only Minecraft server. See your playtime, deaths, kills and more."
       />
 
-      <div>
-        <h1 style={{ textAlign: 'center' }}>
-          The stats page for Streamline SMP is down for this season. Sorry for the inconvenience.
-        </h1>
-
-        <a style={{ textDecoration: 'underline', textAlign: 'center' }} href="./">
-          Go Home
-        </a>
+      <div
+        style={{ height, display: 'grid', alignItems: 'center' }}
+        className={stats.frame}>
+        <p style={{ textAlign: 'center' }}>
+          Opened in new tab, if not click{' '}
+          <a
+            style={{ color: 'lightblue', textDecoration: 'underline' }}
+            href="http://srv34.godlike.club:26914/">
+            here
+          </a>
+          .
+        </p>
       </div>
     </>
   )
